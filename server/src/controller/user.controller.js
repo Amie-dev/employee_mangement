b5f7be1aac6f13eb64cfc03a837dfa295cf97c96
Amie-dev <learn.amie01@gmail.com>
2025-08-29 19:43:33 +0530

    Fix: Update getCurrentUser conroller

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -3,7 +3,17 @@ import ApiError from '../utils/ApiError.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import { User } from '../model/user.model.js';
 import { localFileRemove } from '../middleware/multer.middleware.js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import { deleteFromCloudinary, uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ProjectMember } from '../model/projectmember.model.js';
+import mongoose from 'mongoose';
+
+const options = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'Strict',
+  // maxAge: 7 * 24 * 60 * 60 * 1000,
+};
 
 const generateAccessAndRefreshTokens = async (userId) => {
   const user = await User.findById(userId);
@@ -14,6 +24,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
 
   return { accessToken, refreshToken };
 };
+
 const registerUser = asyncHandler(async (req, res) => {
   const { username, fullName, email, password } = req.body;
 
@@ -34,34 +45,44 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, 'User already exists with this username or email');
   }
 
-  const user = await User.create({
-    username: normalizedUsername,
-    fullName,
-    email,
-    password,
-  });
-  if (avatarLocalPath) {
-    user.avatar.localPath = avatarLocalPath;
+  let avatar;
+
+  try {
+    if (avatarLocalPath) {
+      avatar = await uploadOnCloudinary(avatarLocalPath);
+    }
+    const user = await User.create({
+      username: normalizedUsername,
+      fullName,
+      email,
+      password,
+    });
+    if (avatar) {
+      user.avatar = {
+        url: avatar.url,
+        public_id: avatar.public_id,
+      };
+    }
+    await user.save({ validateBeforeSave: false });
+    // Prepare sanitized response
+    const sanitizedUser = {
+      _id: user._id,
+      username: user.username,
+      fullName: user.fullName,
+      email: user.email,
+      avatar: avatar,
+    };
+    console.log('User created successfully');
+
+    res.status(201).json(new ApiResponse(201, sanitizedUser, 'User created successfully'));
+  } catch (error) {
+    if (avatar) {
+      await deleteFromCloudinary(avatar.public_id);
+    }
+
+    throw new ApiError(500, error.message || 'User registration failed, uploads rolled back');
   }
-  // Prepare sanitized response
-  const sanitizedUser = {
-    _id: user._id,
-    username: user.username,
-    fullName: user.fullName,
-    email: user.email,
-    avatar: user.avatar.localPath,
-  };
-  console.log('User created successfully');
-
-  res.status(201).json(new ApiResponse(201, sanitizedUser, 'User created successfully'));
 });
-
-const options = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Strict',
-  // maxAge: 7 * 24 * 60 * 60 * 1000,
-};
 
 const logInUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
@@ -97,7 +118,7 @@ const logInUser = asyncHandler(async (req, res) => {
 
 const logOutUser = asyncHandler(async (req, res) => {
   if (!req.user || !req.user._id) {
-    throw new ApiError(401, "User not authenticated");
+    throw new ApiError(401, 'User not authenticated');
   }
   await User.findByIdAndUpdate(
     req.user._id,
@@ -113,16 +134,16 @@ const logOutUser = asyncHandler(async (req, res) => {
 
   res
     .status(200)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)  
-    .json(new ApiResponse(200, {}, "User logged out successfully"));
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
+    .json(new ApiResponse(200, {}, 'User logged out successfully'));
 });
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const inComingToken = req.cookies.refreshToken || req.body.refreshToken;
   // console.log("Incoming Refresh Token:", inComingToken);
 
   if (!inComingToken) {
-    throw new ApiError(400, "User login expired. Please log in again.");
+    throw new ApiError(400, 'User login expired. Please log in again.');
   }
 
   const decodedToken = jwt.verify(inComingToken, process.env.REFRESH_TOKEN_SECRET);
@@ -130,60 +151,125 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
   const user = await User.findById(decodedToken._id);
   if (!user) {
-    throw new ApiError(404, "User not found");
+    throw new ApiError(404, 'User not found');
   }
 
   if (inComingToken !== user.refreshToken) {
-    throw new ApiError(403, "Refresh token mismatch");
+    throw new ApiError(403, 'Refresh token mismatch');
   }
 
   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
 
-  
-
   res
     .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(new ApiResponse(200, {}, "Tokens refreshed successfully"));
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(new ApiResponse(200, {}, 'Tokens refreshed successfully'));
 });
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
   const { oldPassword, newPassword, conframdPassword } = req.body;
 
   if (oldPassword === newPassword) {
-    throw new ApiError(401, "New password must be different from the old password");
+    throw new ApiError(401, 'New password must be different from the old password');
   }
 
   if (newPassword !== conframdPassword) {
-    throw new ApiError(400, "New password and confirmation do not match");
+    throw new ApiError(400, 'New password and confirmation do not match');
   }
 
   const user = await User.findById(req.user?._id);
   if (!user) {
-    throw new ApiError(401, "User not authorized");
+    throw new ApiError(401, 'User not authorized');
   }
 
   const isPasswordCorrect = await user.isCorrectPassword(oldPassword);
   if (!isPasswordCorrect) {
-    throw new ApiError(400, "Incorrect current password");
+    throw new ApiError(400, 'Incorrect current password');
   }
 
   user.password = newPassword;
   await user.save({ validateBeforeSave: true });
 
-  res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"));
+  res.status(200).json(new ApiResponse(200, {}, 'Password changed successfully'));
 });
 
-const getCurrentUser=asyncHandler(async(req,res)=>{
+const getCurrentUser = asyncHandler(async (req, res) => {
   if (!req.user || !req.user._id) {
-    throw new ApiError(401, "User not authenticated");
+    throw new ApiError(401, 'User not authenticated');
   }
-  console.log(req.user);
-  
+
+  const currentUser = await ProjectMember.findOne({
+    user: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  if (!currentUser) {
+    return res.status(200).json(new ApiResponse(200, req.user, 'User fetched successfully'));
+  }
+
+  const sanitizedUser = await ProjectMember.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(currentUser._id) },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'user',
+        foreignField: '_id',
+        as: 'userDetails',
+      },
+    },
+    { $unwind: '$userDetails' },
+    {
+      $lookup: {
+        from: 'projects',
+        localField: 'project',
+        foreignField: '_id',
+        as: 'projectDetails',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'createdBy', // project owner reference
+              foreignField: '_id',
+              as: 'projectOwner',
+            },
+          },
+          { $unwind: '$projectOwner' },
+        ],
+      },
+    },
+    { $unwind: '$projectDetails' },
+    {
+      $project: {
+        _id: 0,
+        role: 1,
+        user: {
+          username: '$userDetails.username',
+          email: '$userDetails.email',
+          fullName: '$userDetails.fullName',
+          isEmailVerified: '$userDetails.isEmailVerified',
+          avatar:'$userDetails.avatar.url',
+          createdAt: '$userDetails.createdAt',
+        },
+        project: {
+          projectName: '$projectDetails.name',
+          ownerName: '$projectDetails.projectOwner.fullName',
+        },
+      },
+    },
+  ]);
+
   return res
     .status(200)
-    .json(new ApiResponse(200, req.user, "User fetched successfully"));
-})
+    .json(new ApiResponse(200, sanitizedUser[0] || req.user, 'User fetched successfully'));
+});
 
-export { registerUser, logInUser ,logOutUser ,refreshAccessToken,changeCurrentPassword ,getCurrentUser};
+export {
+  registerUser,
+  logInUser,
+  logOutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+};
